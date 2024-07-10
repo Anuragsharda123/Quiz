6765e7df76f8910b54cdf4eec5d5edd68066b764
Anuragsharda123 <anuragsharda131@gmail.com>
2024-07-10 21:35:01 +0530

    commit

--- Test/views/test_questions.py
+++ Test/views/test_questions.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render, redirect
+from ..models.question import Question
+from ..models.answer import Answer
+from ..models.test import Test
 from django.views import View
+
 
 class Test_Questions(View):
     def get(self, request):
-        return render(request, 'test.html')
+        test = Test.objects.get(id=int(request.session['test']))
+        
+        questions = Question.objects.filter(Test=test).prefetch_related('question')
+        data = {
+            'test': test,
+            'questions': questions,
+            'timing': test.Timing
+            }
+
+            
+
+        return render(request, 'test.html', data)
