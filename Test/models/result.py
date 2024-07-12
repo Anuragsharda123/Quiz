b607ceebc27dd64a4cc9953f45c929aa640e87e4
Anuragsharda123 <anuragsharda131@gmail.com>
2024-07-13 00:15:05 +0530

    commit

--- Test/models/result.py
+++ Test/models/result.py
@@ -5,7 +5,7 @@ from .student import Student
 class Result(models.Model):
     Test = models.ForeignKey(Test, on_delete=models.CASCADE)
     Student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    Rank = models.IntegerField()
+    Rank = models.IntegerField(null=True)
     Total_Marks = models.IntegerField()
     Total_Attempted = models.IntegerField()
     Accuracy = models.IntegerField()
--- Test/views/result.py
+++ Test/views/result.py
@@ -6,13 +6,15 @@ from ..models.student import Student
 from ..models.test import Test
 from ..models.user import User
 from ..models.student_attempt import Student_Attempt
+from ..models.result import Result
 
 class Result(View):
     def get(self, request):
         return redirect('home')
     
     def post(self, request):
-
+        marks = 0
+        correct = 0
         user = User.objects.get(Email=request.session['user'])
         student = Student.objects.get(Email=user)
         test = Test.objects.get(id=int(request.session['test']))
@@ -23,18 +25,27 @@ class Result(View):
                 question_id = key.replace('question', '')
                 answers[question_id] = request.POST.getlist(key)[0]
 
-        
+
+        attempt = len(answers)
 
         print("Hello",answers)
         
         for i,j in answers.items():
             question = Question.objects.get(id=int(i))
             ans = Answer.objects.get(id=int(j))
+
+            if(ans.is_Correct):
+                marks += question.Marks
+                correct += 1
+            else:
+                marks -= (question.Marks/3)
+
             student_attempt = Student_Attempt(Student=student, Test=test, Question=question, Answer=ans)
-            
+            student_attempt.save()
+        
+        acc = (attempt/correct)*100
 
-        return redirect('test_series')
+        result = Result(Test = test, Student=student, Total_Marks = marks, Total_Attempted = attempt, Accuracy = acc )
+        result.save()
 
-
-def set_result():
-    pass
+        return redirect('result')
