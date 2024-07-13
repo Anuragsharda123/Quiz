87b8778ceff74c78f2367fd6e12f1b00565285f6
Anuragsharda123 <anuragsharda131@gmail.com>
2024-07-13 18:29:24 +0530

    commit

--- Test/views/analysis.py
+++ Test/views/analysis.py
@@ -34,19 +34,19 @@ class Analysis(View):
     def post(self, request):
         rank = 1
         t = request.POST.get('testid')
-        test = Test.objects.get(id = int(t))
+        test = Test.objects.get(id = t)
         user = User.objects.get(Email=request.session['user'])
         student = Student.objects.get(Email=user)
         attempted_student = Result.objects.get(Student=student, Test=test)
-        attempted_students = Result.objects.all().exclude(Student=student, Test=test)
+        attempted_students = Result.objects.filter(Test=test).exclude(Student=student)
         number_of_students = attempted_students.count() + 1
 
         for i in attempted_students:
             if attempted_student.Total_Marks <= i.Total_Marks:
                 rank += 1
-        
+            
         attempted_student.Rank = rank
-        
+            
         percentile = ((number_of_students-(rank-1))/number_of_students)*100
         attempted_student.Percentile = percentile
         attempted_student.save()
