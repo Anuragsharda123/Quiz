60e77e14a384d974f087301e3d7691b011bb9322
Anuragsharda123 <anuragsharda131@gmail.com>
2024-07-19 17:03:22 +0530

    commit-vercel-1

--- Quiz/settings.py
+++ Quiz/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-o-0(#5=jlhj!s_@x*4li+oj+qwco+gluqrhka@549p5a_hhn6z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '.vercel.app']
 
 
 # Application definition
