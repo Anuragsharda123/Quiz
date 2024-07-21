8f3f1b7d2b15d931ad2704ada6ab7c78edce39c1
Anuragsharda123 <anuragsharda131@gmail.com>
2024-07-21 18:21:31 +0530

    commit

--- Quiz/settings.py
+++ Quiz/settings.py
@@ -10,15 +10,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-o-0(#5=jlhj!s_@x*4li+oj+qwco+gluqrhka@549p5a_hhn6z'
-SECRET_KEY = '_s7---k_u6c=(pweg-cpbm_kfwvi6%&^fly))#1&2@8!a0xozu'
-# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-database_url = "postgresql://quiz_wexf_user:kvToxtMgq7AMIzv1owFQELwwmUJuQm0U@dpg-cqeeok9u0jms739a5j70-a.oregon-postgres.render.com/quiz_wexf"
-ALLOWED_HOSTS = ['*', '.vercel.app', '.now.sh']
-# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+
+# ALLOWED_HOSTS = ['*', '.vercel.app', '.now.sh']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -85,8 +84,9 @@ DATABASES = {
     }
 }
 
-
+database_url = os.environ.get("DATABASE_URL")
 DATABASES['default'] = dj_database_url.parse(database_url)
+# DATABASES['default'] = dj_database_url.parse("postgresql://quiz_wexf_user:kvToxtMgq7AMIzv1owFQELwwmUJuQm0U@dpg-cqeeok9u0jms739a5j70-a.oregon-postgres.render.com/quiz_wexf")
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
